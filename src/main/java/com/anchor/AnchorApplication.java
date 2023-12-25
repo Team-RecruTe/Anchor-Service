a47eaf4b997553e3794748b3325094dbfe82c148
hossi97 <asd8500@naver.com>
2023-12-25 21:27:09 +0900

    chore: 프로젝트 생성 및 초기화
    - gitignore 적용
    - 의존성 추가

---
+++ src/main/java/com/anchor/AnchorApplication.java
@@ -0,0 +1,13 @@
+package com.anchor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnchorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnchorApplication.class, args);
+  }
+
+}
