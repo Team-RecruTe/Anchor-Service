4957fd1c13522fca90c4d1228115984fbbb938e8
hossi97 <asd8500@naver.com>
2024-01-02 16:18:58 +0900

    feature: 멘토링 관리 기능 코드 보완 - 비즈니스 로직과 도메인 로직 분리 - 중복되는 코드 메서드로 추출 - DB의존성 테스트 진행

--- src/test/java/com/anchor/domain/mentoring/api/service/MentoringServiceTest.java
+++ src/test/java/com/anchor/domain/mentoring/api/service/MentoringServiceTest.java
@@ -57,6 +57,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -492,5 +493,5 @@ class MentoringServiceTest {
 
     return sessionList;
   }
-}
 
+}
