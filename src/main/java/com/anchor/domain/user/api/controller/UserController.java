fb38802efab3c51b1d08528f225de0315b639171
2JISUN <111330178+2JISUN@users.noreply.github.com>
2024-01-10 10:34:20 +0900

    feature: 회원 정보 기능 구현  (#22)
    - 나의 회원 정보 페이지 조회
    - 나의 회원 정보 수정
    - 회원 탈퇴

--- src/main/java/com/anchor/domain/user/api/controller/UserController.java
+++ src/main/java/com/anchor/domain/user/api/controller/UserController.java
@@ -1,27 +1,56 @@
 package com.anchor.domain.user.api.controller;
 
 import com.anchor.domain.user.api.controller.request.MentoringStatusInfo;
+import com.anchor.domain.user.api.controller.request.UserNicknameRequest;
 import com.anchor.domain.user.api.service.UserService;
 import com.anchor.domain.user.api.service.response.AppliedMentoringInfo;
+import com.anchor.domain.user.api.service.response.UserInfoResponse;
 import com.anchor.global.auth.SessionUser;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping
+@RestController("/users")
 public class UserController {
 
   private static final String SUCCESS = "success";
   private static final String FAILURE = "failure";
   private final UserService userService;
+
+  /**
+   *  유저 정보 변경
+   */
+  @PutMapping("/me")
+  public ResponseEntity<String> putInfo(@RequestBody UserNicknameRequest userNicknameRequest, HttpSession httpSession){
+    SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+    userService.editNickname(sessionUser.getEmail(), userNicknameRequest);
+    return ResponseEntity.ok().build();
+  }
+
+  @PutMapping("/me/image")
+  public ResponseEntity<String> putImage(@RequestBody UserNicknameRequest userNicknameRequest, HttpSession httpSession){
+    return ResponseEntity.ok().build();
+  }
+
+  @DeleteMapping("/me")
+  public ResponseEntity<String> deleteUser(HttpSession httpSession){
+    SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user"); //email, nickname, image
+    userService.deleteUser(sessionUser.getEmail());
+    return ResponseEntity.ok().build();
+  }
+
 
   /**
    * 멘토링 신청내역을 조회합니다.
@@ -60,4 +89,6 @@ public class UserController {
     }
     return sessionUser;
   }
+
+
 }
--- src/main/java/com/anchor/domain/user/api/service/UserService.java
+++ src/main/java/com/anchor/domain/user/api/service/UserService.java
@@ -31,6 +31,34 @@ public class UserService {
   private final PaymentRepository paymentRepository;
   private final ExternalApiUtil apiUtil;
 
+  @Transactional
+  public UserInfoResponse getProfile(String email){
+    User user = userRepository.findByEmail(email)
+        .orElseThrow(()->{
+          return new RuntimeException("해당 유저를 찾을 수 없습니다.");
+        });
+    return new UserInfoResponse(user);
+  }
+
+  @Transactional
+  public void editNickname(String email, UserNicknameRequest userNicknameRequest){
+    User user = userRepository.findByEmail(email)
+        .orElseThrow(()->{
+          return new RuntimeException("해당 유저를 찾을 수 없습니다.");
+        });
+    user.editNickname(userNicknameRequest);
+    userRepository.save(user);
+  }
+
+  @Transactional
+  public void deleteUser(String email){
+    User user = userRepository.findByEmail(email)
+        .orElseThrow(()->{
+          return new RuntimeException("해당 유저를 찾을 수 없습니다.");
+        });
+    userRepository.delete(user);
+  }
+
   @Transactional(readOnly = true)
   public List<AppliedMentoringInfo> loadAppliedMentoringList(SessionUser sessionUser) {
 
