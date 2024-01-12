ac391bcd9d5cbe2ea23fef0d07490537480734c2
Kihong <kks4517@naver.com>
2024-01-12 16:35:59 +0900

    feature: Redis 의존성 추가 및 설정

---
+++ src/main/java/com/anchor/global/config/RedisConfig.java
@@ -0,0 +1,56 @@
+package com.anchor.global.config;
+
+import com.anchor.global.util.type.DateTimeRange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+@EnableRedisRepositories
+public class RedisConfig {
+
+
+  @Value("${spring.data.redis.session.host}")
+  private String hostName;
+
+  @Value("${spring.data.redis.session.port}")
+  private int sessionPort;
+
+  @Value("${spring.data.redis.storage.port}")
+  private int storagePort;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setHostName(hostName);
+    redisStandaloneConfiguration.setPort(sessionPort);
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
+  }
+
+  @Bean(name = "redisStorageConnectionFactory")
+  public RedisConnectionFactory redisStorageConnectionFactory() {
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setHostName(hostName);
+    redisStandaloneConfiguration.setPort(storagePort);
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
+  }
+
+  @Bean
+  public RedisTemplate<String, DateTimeRange> redisTemplate() {
+    RedisTemplate<String, DateTimeRange> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisStorageConnectionFactory());
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    return redisTemplate;
+  }
+
+}
