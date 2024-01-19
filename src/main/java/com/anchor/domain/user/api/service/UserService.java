72c8aa55be374824b1ce1b1602c65fa151a9a51a
LIM JAEHO <58262954+hossi97@users.noreply.github.com>
2024-01-19 14:49:14 +0900

    feature: 스케줄링을 통한 s3 이미지 삭제 구현 (#41)
    - 스케줄링을 통한 s3 이미지 삭제

--- src/main/java/com/anchor/domain/user/api/service/UserService.java
+++ src/main/java/com/anchor/domain/user/api/service/UserService.java
@@ -37,6 +37,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
