257a9cb6387ab90cca9db092f213a1963f312f3a
2JISUN <snm03097@gmail.com>
2024-01-22 14:49:19 +0900

    git commit -m " refactor: 충돌 수정
    "

--- src/main/java/com/anchor/global/auth/SessionUser.java
+++ src/main/java/com/anchor/global/auth/SessionUser.java
@@ -34,14 +34,6 @@ public class SessionUser implements Serializable {
     return sessionUser;
   }
 
-  public static SessionUser getSessionUser(HttpSession session) {
-    SessionUser sessionUser = (SessionUser) session.getAttribute("user");
-    if (sessionUser == null) {
-      throw new RuntimeException("로그인 정보가 존재하지 않습니다.");
-    }
-    return sessionUser;
-  }
-
   public void addMentorId(Long mentorId) {
     this.mentorId = mentorId;
   }
