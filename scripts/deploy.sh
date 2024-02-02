a4ce48e8890c538f7f00a67d517033cb08d45bf2
hossi97 <asd8500@naver.com>
2024-02-02 11:42:49 +0900

    chore: CD 환경 구성을 위한  설정 추가

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## Set Variables
+APP_NAME="anchor"
+DEV_DIR="/home/ubuntu/$APP_NAME/dev"
+JAR_NAME=$(ls $DEV_DIR/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH="$DEV_DIR/build/libs/$JAR_NAME"
+ENV_PATH="$DEV_DIR/env"
+
+APP_LOG="$DEV_DIR/log/app.log"
+ERROR_LOG="$DEV_DIR/log/error.log"
+
+## Stop Application
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> Application Not Found: 종료할 어플리케이션이 없습니다."
+else
+  echo "> Stop Application: 실행중인 어플리케이션을 종료합니다."
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+## Start Application
+NOW=$(date +%c)
+
+echo "> Start Application: 어플리케이션을 실행합니다. // $JAR_PATH ($NOW)"
+source $ENV_PATH/env.sh
+nohup java -jar $JAR_PATH > $APP_LOG 2> $ERROR_LOG &
