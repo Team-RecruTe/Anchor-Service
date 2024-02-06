d6bf47ae27aa43b5b5ade15170f5a94482be8c9a
Kihong <kks4517@naver.com>
2024-02-07 01:03:44 +0900

    refactor: MentoringStatus find 메서드 수정

--- src/main/java/com/anchor/domain/mentoring/domain/MentoringStatus.java
+++ src/main/java/com/anchor/domain/mentoring/domain/MentoringStatus.java
@@ -3,10 +3,6 @@ package com.anchor.domain.mentoring.domain;
 import com.anchor.global.exception.type.entity.MentoringStatusNotFoundException;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -19,18 +15,16 @@ public enum MentoringStatus {
   APPROVAL("승인"),
   COMPLETE("완료");
 
-  private static final Map<String, MentoringStatus> MENTORING_STATUS_MAP = Arrays.stream(MentoringStatus.values())
-      .collect(Collectors.toMap(MentoringStatus::getDescription, Function.identity()));
-
   @JsonValue
   private final String description;
 
   @JsonCreator
   public static MentoringStatus find(String name) {
-    if (MENTORING_STATUS_MAP.containsKey(name)) {
-      return MENTORING_STATUS_MAP.get(name);
+    try {
+      return MentoringStatus.valueOf(name.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      throw new MentoringStatusNotFoundException();
     }
-    throw new MentoringStatusNotFoundException();
   }
 
   public boolean isEqualTo(String status) {
